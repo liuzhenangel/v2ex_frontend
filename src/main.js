005db7b22fbaa0cafc2cf38a9d225d9b0abfafa9
liuzhenangel <550686383@qq.com>
2016-09-14 22:33:40 +0800

    rm moment package

--- src/main.js
+++ src/main.js
@@ -1,18 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
+import VuePaginate from 'vue-paginate'
+
 import { formatDate } from './filters'
 import App from './components/App.vue'
 import Index from './components/Index.vue'
 import Test from './components/test.vue'
 import Show from './components/Show.vue'
 import Member from './components/Member.vue'
-import VueMoment from 'moment'
-import VuePaginate from 'vue-paginate'
 
 Vue.use(VueResource)
 Vue.use(VueRouter)
-Vue.use(VueMoment)
 Vue.use(VuePaginate)
 
 Vue.filter('formatDate', formatDate)
