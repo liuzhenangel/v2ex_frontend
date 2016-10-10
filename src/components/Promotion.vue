d31838e5d7dad9368a7e04d3ca75bb779cf64b31
liuzhenangel <550686383@qq.com>
2016-10-10 09:15:34 +0800

    add ruby-china.liuzhen.me link

--- src/components/Promotion.vue
+++ src/components/Promotion.vue
@@ -3,6 +3,9 @@
     <div class="panel-heading">推广</div>
     <ul class='uk-nav uk-nav-side'>
       <li>
+        <a href='http://ruby-china.liuzhen.me/' target='_blank'>RubyChina 克隆</a>
+      </li>
+      <li>
         <a href='http://liuzhen.me/about' target='_blank'>个人简历</a>
       </li>
       <li>
@@ -13,6 +16,9 @@
       </li>
       <li>
         <a href='https://github.com/liuzhenangel/v2ex_frontend' target='_blank'>V2EX Code</a>
+      </li>
+      <li>
+        <a href='https://github.com/liuzhenangel/react-ruby-china' target='_blank'>RubyChina Code</a>
       </li>
       <li>
         <a href='https://github.com/liuzhenangel/Rina_Blog' target='_blank'>Blog Code</a>
